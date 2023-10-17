cb23cf47ed910de9ee4d832e9e276ec4bdad64a5
Jack Hau <jhh23@imperial.ac.uk>
2023-10-17 15:53:29 +0100

    added decision_tree.py

---
+++ decision_tree.py
@@ -0,0 +1,19 @@
+import numpy, matplotlib
+
+class DecisionTree:
+    
+    def __init__(self, data):
+        self.data = data
+        
+    def decision_tree_learning(self):
+        pass
+    
+    def find_split(self):
+        pass
+    
+    
+    
+if __name__ == "__main__":
+    decision_tree = DecisionTree()
+    
+    
