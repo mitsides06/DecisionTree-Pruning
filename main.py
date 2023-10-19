767df29809e9fca434ef22587a7c6ad35d9f31ba
Konstantinos Mitsides <konstantinos.mitsides23@imperial.ac.uk>
2023-10-19 15:51:33 +0100

    Commented on find_split function, changed the return value of decision_tree_learning function, see comments attached

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def find_split(data):
 
     for feature in range(data.shape[1] - 1):
         unique_values = np.unique(data[:, feature])
-        for value in unique_values:
+        for value in unique_values:  # we don't need to go over the last unique value as the right split will be empty
             left_split = data[data[:, feature] <= value]
             right_split = data[data[:, feature] > value]
 
@@ -38,7 +38,7 @@ def find_split(data):
 
 def decision_tree_learning(data, depth=0):
     if len(np.unique(data[:, -1])) == 1:
-        return {"leaf": True, "class": data[0, -1], "depth": depth}
+        return ({"leaf": True, "class": data[0, -1]}, depth)  # removed "depth" from the dictionary, and put it as an element in a 2-tuple as our function should return (node, depth)
 
     feature, value = find_split(data)
     left_data = data[data[:, feature] <= value]
@@ -47,14 +47,15 @@ def decision_tree_learning(data, depth=0):
     left_branch, left_depth = decision_tree_learning(left_data, depth+1)
     right_branch, right_depth = decision_tree_learning(right_data, depth+1)
 
-    return {
+    return ({
         "leaf": False,
         "feature": feature,
         "value": value,
         "left": left_branch,
-        "right": right_branch,
-        "depth": max(left_depth, right_depth)
-    }
+        "right": right_branch
+        }, 
+        max(left_depth, right_depth)
+        )  # did the same thing as above. I am not quite sure about the styling convection here tho
 
 
 # Step 3: Evaluation
